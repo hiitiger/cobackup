d80dd1d1df41f470b785d6bfe4ac337db860f27f
hiitiger <lushang@outlook.com>
2023-03-24 20:17:16 +0800

    Add files via upload

---
+++ share/Profile.ps1
@@ -0,0 +1,5 @@
+Invoke-Expression (&starship init powershell)
+# Reverse Search
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
